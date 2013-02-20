0ccff749a19d7a08b8344541bb46aa7a1d54b726
Alexey Zankevich <zancudero@gmail.com>
2013-02-20 16:57:15 +0300

    Updated main scene

---
+++ hello-world/main.lua
@@ -0,0 +1,38 @@
+local terevaka = require("terevaka.terevaka")
+
+MyApplication = terevaka.TKApplication:new()
+
+function MyApplication:onCreate()
+   print('onCreate')
+end
+
+function MyApplication:onStart()
+   print('onStart')
+end
+
+function MyApplication:onResume()
+   print('onResume')
+end
+local app = MyApplication:new()
+terevaka.TKApplication:setSharedApp(app) --   <-- call this if you want to access later to your application as a global variable
+
+
+MainScene = terevaka.TKScene:new()
+
+function MainScene:init()
+   -- loading sprite pack
+   self.texturePack = terevaka.TKResourceManager.loadTexturePack('main')
+   
+   -- loading scene layer
+   self.layer = MOAILayer2D.new()
+   self:fillLayer(self.layer, self.texturePack, 'main-layout')
+end
+
+function MainScene:getLayers()
+   return {self.layer}
+end
+
+local mainScene = MainScene:new()
+mainScene:init()
+
+app:initWithScene(mainScene)
