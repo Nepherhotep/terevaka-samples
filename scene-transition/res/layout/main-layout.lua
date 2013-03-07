fd9070254fcba5e4a44d456720de7c66713486c7
Alexey Zankevich <zancudero@gmail.com>
2013-03-07 18:41:57 +0300

    Added scene transition example

---
+++ scene-transition/res/layout/main-layout.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        ["align_left"] = true,
+        ["name"] = "sprite2.png",
+        ["x_unit"] = "%",
+        ["y_unit"] = "%",
+        ["align_bottom"] = true,
+        ["y"] = 50.0,
+        ["x"] = 50.0,
+        ["type"] = "sprite",
+        ["id"] = 1,
+        ["uid"] = "pink_box",
+    }
+}
