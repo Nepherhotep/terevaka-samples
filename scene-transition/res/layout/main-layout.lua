091c0ad3c439870cf97b47bb7ae7f5e21823a319
Alexey Zankevich <zancudero@gmail.com>
2013-03-13 12:15:32 +0300

    regenerated examples layouts

--- scene-transition/res/layout/main-layout.lua
+++ scene-transition/res/layout/main-layout.lua
@@ -1,14 +1,16 @@
 return {
-    {
-        ["align_left"] = true,
-        ["name"] = "sprite2.png",
-        ["x_unit"] = "%",
-        ["y_unit"] = "%",
-        ["align_bottom"] = true,
-        ["y"] = 50.0,
-        ["x"] = 50.0,
-        ["type"] = "sprite",
-        ["id"] = 1,
-        ["uid"] = "pink_box",
+    ["props"] = {
+        {
+            ["align_bottom"] = true,
+            ["name"] = "sprite2.png",
+            ["align_left"] = true,
+            ["y_unit"] = "%",
+            ["x_unit"] = "%",
+            ["y"] = 50.0,
+            ["x"] = 50.0,
+            ["type"] = "sprite",
+            ["id"] = 1,
+            ["uid"] = "pink_box",
+        }
     }
 }
