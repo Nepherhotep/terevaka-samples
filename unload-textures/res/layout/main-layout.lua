681b5f70f00833e162d4128ab8a105ed5c0e4e89
Alexey Zankevich <zancudero@gmail.com>
2013-03-27 11:59:45 +0300

    uploadted uload-textures main layout

--- unload-textures/res/layout/main-layout.lua
+++ unload-textures/res/layout/main-layout.lua
@@ -7,10 +7,11 @@ return {
             ["align_left"] = true,
             ["y_unit"] = "%",
             ["x_unit"] = "%",
-            ["y"] = 50.0,
-            ["x"] = 50.0,
+            ["y"] = 50,
+            ["x"] = 50,
             ["type"] = "sprite",
             ["id"] = 1,
+            ["uid"] = "open_second_scene",
         },
         {
             ["align_bottom"] = false,
