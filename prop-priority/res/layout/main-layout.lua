dce534bded8ffd4aec17d93d8145b0dcc5897850
Alexey Zankevich <zancudero@gmail.com>
2013-04-24 20:30:24 +0300

    Added a new sample

---
+++ prop-priority/res/layout/main-layout.lua
@@ -0,0 +1,43 @@
+return {
+    ["layout_type"] = "scalable",
+    ["layout_width"] = 1024,
+    ["layout_height"] = 768,
+    ["props"] = {
+        {
+            ["z_index"] = 10.0,
+            ["name"] = "sprite3.png",
+            ["h_align"] = "center",
+            ["y"] = 509.620817844,
+            ["x"] = 459.657992565,
+            ["type"] = "sprite",
+            ["id"] = 5,
+        },
+        {
+            ["z_index"] = 0,
+            ["name"] = "sprite4.png",
+            ["h_align"] = "center",
+            ["y"] = 465.36802974,
+            ["x"] = 502.483271375,
+            ["type"] = "sprite",
+            ["id"] = 6,
+        },
+        {
+            ["z_index"] = -10.0,
+            ["name"] = "sprite2.png",
+            ["h_align"] = "center",
+            ["y"] = 421.115241636,
+            ["x"] = 552.446096654,
+            ["type"] = "sprite",
+            ["id"] = 7,
+        },
+        {
+            ["z_index"] = -20.0,
+            ["name"] = "sprite1.png",
+            ["h_align"] = "center",
+            ["y"] = 378.289962825,
+            ["x"] = 598.126394052,
+            ["type"] = "sprite",
+            ["id"] = 8,
+        }
+    }
+}
