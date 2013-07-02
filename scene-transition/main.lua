29a3da58fc1f1309d897e49002dd67245440e23b
Alexey Zankevich <zancudero@gmail.com>
2013-07-02 12:39:29 +0300

    updated scene transition sample

--- scene-transition/main.lua
+++ scene-transition/main.lua
@@ -10,61 +10,61 @@ function MainScene:init()
    self.texturePack = terevaka.TKResourceManager.loadTexturePack('main')
    
    -- loading scene layer
-   self.layer = MOAILayer2D.new()
-   self:fillLayer({layer = self.layer, resourceName='main-layout', texturePack = self.texturePack})
+   self.layer = terevaka.TKLayer:new () :init ()
+   self.layer:fill ({ resourceName='main-layout', texturePack = self.texturePack })
 end
 
 function MainScene:onTouch(event)
    self:handleTouch(self.layer, event)
 end
 
-function MainScene:getRenderTable()
-   return {self.layer}
+function MainScene:getRenderTable ()
+   return { self.layer:getMOAILayer ()}
 end
 
-local mainScene = MainScene:new()
-mainScene:init()
+local mainScene = MainScene:new ()
+mainScene:init ()
 
 
-SecondScene = terevaka.TKScene:new()
+SecondScene = terevaka.TKScene:new ()
 -- Creating second scene
-function SecondScene:init()
+function SecondScene:init ()
    -- loading sprite pack
    self.texturePack = terevaka.TKResourceManager.loadTexturePack('main')
    
    -- loading scene layer
-   self.layer = MOAILayer2D.new()
-   self:fillLayer({layer = self.layer, resourceName='second-layout', texturePack = self.texturePack})
+   self.layer = terevaka.TKLayer:new () :init ()
+   self.layer:fill ({ resourceName='second-layout', texturePack = self.texturePack })
 end
 
-function SecondScene:onTouch(event)
+function SecondScene:onTouch ( event )
    -- any touch switches to main scene
-   local app = terevaka.TKApplication:getSharedApp()
-   app:replaceScene(mainScene)
+   local app = terevaka.TKApplication:getSharedApp ()
+   app:replaceScene ( mainScene )
 end
 
-function SecondScene:getRenderTable()
-   return {self.layer}
+function SecondScene:getRenderTable ()
+   return { self.layer:getMOAILayer ()}
 end
 
-local secondScene = SecondScene:new()
-secondScene:init()
+local secondScene = SecondScene:new ()
+secondScene:init ()
 
 
 -- Creating my application
-function MyApplication:onCreate()
-   print('onCreate')
+function MyApplication:onCreate ()
+   print ( 'onCreate' )
 end
 
-function MyApplication:onResume()
-   print('onResume')
+function MyApplication:onResume ()
+   print ( 'onResume' )
 end
 
-local app = MyApplication:new()
-terevaka.TKApplication:setSharedApp(app) --   <-- call this if you want to access later to your application as a global variable
-app:initWithScene(mainScene)
+local app = MyApplication:new ()
+terevaka.TKApplication:setSharedApp ( app ) --   <-- call this if you want to access later to your application as a global variable
+app:initWithScene ( mainScene )
 
-local pinkBox = mainScene:findPropById('main-layout', 'pink_box')
-pinkBox.onTouch = function(self, event)
-   app:replaceScene(secondScene)
+local pinkBox = mainScene.layer:findPropById ( 'pink_box' )
+pinkBox.onTouch = function ( self, event )
+   app:replaceScene ( secondScene )
 end
