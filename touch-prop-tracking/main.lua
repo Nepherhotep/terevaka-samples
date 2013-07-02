ed5dbb9438d36f2200bf0124d89a33c2526becc7
Alexey Zankevich <zancudero@gmail.com>
2013-07-02 13:09:29 +0300

    update touch prop tracking sample

--- touch-prop-tracking/main.lua
+++ touch-prop-tracking/main.lua
@@ -1,59 +1,59 @@
-local terevaka = require("terevaka.terevaka")
+local terevaka = require ( "terevaka.terevaka" )
 
-MyApplication = terevaka.TKApplication:new()
+MyApplication = terevaka.TKApplication:new ()
 
-MainScene = terevaka.TKScene:new()
+MainScene = terevaka.TKScene:new () :init ()
 
 -- Creating main scene
-function MainScene:init()
+function MainScene:init ()
    -- loading sprite pack
-   self.texturePack = terevaka.TKResourceManager.loadTexturePack('main')
+   self.texturePack = terevaka.TKResourceManager.loadTexturePack ( 'main' )
    
    -- loading scene layer
-   self.layer = MOAILayer2D.new()
-   self:fillLayer({layer = self.layer, resourceName='main-layout', texturePack = self.texturePack})
+   self.layer = terevaka.TKLayer:new () :init ()
+   self.layer:fill ({ resourceName='main-layout', texturePack = self.texturePack })
 end
 
-function MainScene:onTouch(event)
-   self:handleTouch(self.layer, event)
+function MainScene:onTouch ( event )
+   self:handleTouch ( self.layer, event )
 end
 
-function MainScene:getRenderTable()
-   return {self.layer}
+function MainScene:getRenderTable ()
+   return { self.layer:getMOAILayer ()}
 end
 
-local mainScene = MainScene:new()
-mainScene:init()
+local mainScene = MainScene:new ()
+mainScene:init ()
 
 -- Creating my application
-function MyApplication:onCreate()
-   print('onCreate')
+function MyApplication:onCreate ()
+   print ( 'onCreate' )
 end
 
-function MyApplication:onResume()
-   print('onResume')
+function MyApplication:onResume ()
+   print ( 'onResume' )
 end
 
-local app = MyApplication:new()
-terevaka.TKApplication:setSharedApp(app) --   <-- call this if you want to access later to your application as a global variable
-app:initWithScene(mainScene)
+local app = MyApplication:new ()
+terevaka.TKApplication:setSharedApp ( app ) --   <-- call this if you want to access later to your application as a global variable
+app:initWithScene ( mainScene )
 
-local pinkBox = mainScene:findPropById('main-layout', 'pink_box')
-pinkBox.onTouch = function(self, event)
-   self:moveRot( -180, 1.5 )
+local pinkBox = mainScene.layer:findPropById ( 'pink_box' )
+pinkBox.onTouch = function ( self, event )
+   self:moveRot (  -180, 1.5 )
 end
 
-local greenBox = mainScene:findPropById('main-layout', 'green_box')
-greenBox.onTouch = function(self, event)
-   self:moveRot( 180, 1.5 )
+local greenBox = mainScene.layer:findPropById ( 'green_box' )
+greenBox.onTouch = function ( self, event )
+   self:moveRot (  180, 1.5 )
 end
 
-local blueBox = mainScene:findPropById('main-layout', 'blue_box')
-blueBox.onTouch = function(self, event)
-   self:moveRot( -360, 1.5 )
+local blueBox = mainScene.layer:findPropById ( 'blue_box' )
+blueBox.onTouch = function ( self, event )
+   self:moveRot (  -360, 1.5 )
 end
 
-local brownBox = mainScene:findPropById('main-layout', 'brown_box')
-brownBox.onTouch = function(self, event)
-   self:moveRot( 360, 1.5 )
+local brownBox = mainScene.layer:findPropById ( 'brown_box' )
+brownBox.onTouch = function ( self, event )
+   self:moveRot (  360, 1.5 )
 end
