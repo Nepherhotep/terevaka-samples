4ac6dbf8e5708776e71fe943f1b7b4bd3b8ca753
Alexey Zankevich <zancudero@gmail.com>
2013-07-02 12:33:14 +0300

    updated sample find-prop-by-id

--- find-view-by-id/main.lua
+++ find-view-by-id/main.lua
@@ -1,38 +1,39 @@
-local terevaka = require("terevaka.terevaka")
+local terevaka = require( "terevaka.terevaka" )
+local terevaka = require( "terevaka.terevaka" )
 
-MyApplication = terevaka.TKApplication:new()
+MyApplication = terevaka.TKApplication:new ()
 
-MainScene = terevaka.TKScene:new()
+MainScene = terevaka.TKScene:new ()
 
 -- Creating main scene
-function MainScene:init()
+function MainScene:init ()
    -- loading sprite pack
-   self.texturePack = terevaka.TKResourceManager.loadTexturePack('main')
+   self.texturePack = terevaka.TKResourceManager.loadTexturePack ( 'main' )
    
    -- loading scene layer
-   self.layer = MOAILayer2D.new()
-   self:fillLayer({layer = self.layer, resourceName='main-layout', texturePack = self.texturePack})
+   self.layer = terevaka.TKLayer:new () :init ()
+   self.layer:fill ({ resourceName='main-layout', texturePack = self.texturePack })
 end
 
-function MainScene:getRenderTable()
-   return {self.layer}
+function MainScene:getRenderTable ()
+   return { self.layer:getMOAILayer () }
 end
 
-local mainScene = MainScene:new()
-mainScene:init()
+local mainScene = MainScene:new ()
+mainScene:init ()
 
 -- Creating my application
-function MyApplication:onCreate()
-   print('onCreate')
+function MyApplication:onCreate ()
+   print ( 'onCreate' )
 end
 
-function MyApplication:onResume()
-   print('onResume')
+function MyApplication:onResume ()
+   print ( 'onResume' )
 end
 
-local app = MyApplication:new()
-terevaka.TKApplication:setSharedApp(app) --   <-- call this if you want to access later to your application as a global variable
-app:initWithScene(mainScene)
+local app = MyApplication:new ()
+terevaka.TKApplication:setSharedApp ( app ) --   <-- call this if you want to access later to your application as a global variable
+app:initWithScene ( mainScene )
 
-local prop = mainScene:findPropById('main-layout', 'pink_box')
+local prop = mainScene.layer:findPropById ( 'pink_box' )
 prop:moveRot( 360, 1.5 )
